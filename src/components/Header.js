1aebcd909bde1ffd3f277f8c0769a9fdb957034d
JazzDan <danielfedoruck@gmail.com>
2025-04-05 18:31:01 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Ruddit</span>
+            <ul className='nav'>
+                <li>
+                    Сосал?
+                </li>
+                <li>
+                    Про то, какие мы сигмы
+                </li>
+            </ul>
+        </div>
+        <div className='waltuh'></div>
+    </header>
+  )
+}
