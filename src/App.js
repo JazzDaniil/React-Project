cac8241cc7e16bc0a5745b4471d37633c1c45955
JazzDan <danielfedoruck@gmail.com>
2025-04-06 21:17:00 +0300

    Добавил два предмета для продажи (плейсхолдеры)
    Ничего не работает после создания конструктора в App.js (вроде бы)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Items from "./components/Items"
 
-function App() {
-  return (
-    <div className='wrapper'>
-      <Header/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component() {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [
+          {
+            id: 1,
+            title: 'Haram',
+            img: 'walter_haram.png',
+            desc: 'Haram',
+            category: 'Breaking_Bad',
+            price: 'one halon of super milk'
+          },
+          {
+            id: 2,
+            title: 'Super Earth',
+            img: 'Super_Earth.jpg',
+            desc: 'Our home. Liberty, Prosperity, Democracy - our way of live',
+            category: 'Helldivers_2',
+            price: 'cup of liber-tea'
+          }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header/>
+        <Items items={this.state.items} />
+        <Footer/>
+      </div>
+    )
+  }
 }
 
 export default App;
